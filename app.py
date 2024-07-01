8ff82e85b0596b4cc687bd05b8c3995fdbf670b9
Laura Zander <laura.z@hotmail.be>
2024-07-01 14:38:57 +0200

    Ajout de transformers aux requirements et modification de l'app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
+import os
 
 app = Flask(__name__)
 
 # Charger le modèle de NLP
 nlp_model = pipeline('conversational', model="facebook/blenderbot-400M-distill")
+
+@app.route('/')
+def home():
+    return "Bienvenue sur le Chatbot !"
 
 @app.route('/chatbot', methods=['POST'])
 def chatbot_response():
@@ -13,10 +18,10 @@ def chatbot_response():
     
     # Générer une réponse à partir du modèle NLP
     conversation = nlp_model(user_input)
-    response = conversation[0]['generated_text']
+    response = conversation[0]['generated_responses'][0]
     
     return jsonify({"response": response})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
